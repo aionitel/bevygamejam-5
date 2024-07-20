fa0b0e3d689730ee61f66ad2e9197401ea2b0fb3
Alex Ionitel <oranggsoda@gmail.com>
2024-07-20 17:10:05 -0400

    spawning window and camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello Bevy Game Jam 5!");
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins
+        .set(ImagePlugin::default_nearest())
+        .set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Game Jam 5".into(),
+                resolution: (1280., 720.).into(),
+                ..default()
+            }),
+            ..default()
+        }),
+    );
+    app.add_systems(Startup, setup);
+
+    app.run();
+}
+
+fn setup(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2dBundle::default());
 }
